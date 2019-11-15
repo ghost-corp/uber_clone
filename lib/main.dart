9a7e81f192bb868054faaea30ee9408b53f4159a
Abdulmalik <abumuhab98@gmail.com>
2019-11-15 18:36:39 +0100

    bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -36,126 +36,124 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Container(
         color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+        child: ListView(
           children: <Widget>[
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height:  MediaQuery.of(context).size.height*0.68,
-              child:  Image.asset("images/welcomeIcon.jpg",fit: BoxFit.fill,),
-            ),
-            Padding(
-              padding: EdgeInsets.all( MediaQuery.of(context).size.width*0.07),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width*0.07,
-                      height: MediaQuery.of(context).size.width*0.07,
-                      child: Image.asset("images/nigeria.png"),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
-                    child: Text(
-                      "+234",
-                      style:
-                          TextStyle(fontSize: MediaQuery.of(context).size.width*0.05, fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.025),
-                    child: GestureDetector(
-                      onTap: ()
-                      {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => PhoneNumber()));
-                      },
-                      child: Text(
-                        "Enter your mobile number",
-                        style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.width*0.05,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.grey),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  child: Divider(),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height:  MediaQuery.of(context).size.height*0.68,
+                  child:  Image.asset("images/welcomeIcon.jpg",fit: BoxFit.fill,),
                 ),
-                Text("Or connect with social"),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  child: Divider(),
-                )
+                Padding(
+                  padding: EdgeInsets.all( MediaQuery.of(context).size.width*0.07),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(),
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width*0.07,
+                          height: MediaQuery.of(context).size.width*0.07,
+                          child: Image.asset("images/nigeria.png"),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
+                        child: Text(
+                          "+234",
+                          style:
+                          TextStyle(fontSize: MediaQuery.of(context).size.width*0.05, fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.025),
+                        child: GestureDetector(
+                          onTap: ()
+                          {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => PhoneNumber()));
+                          },
+                          child: Text(
+                            "Enter your mobile number",
+                            style: TextStyle(
+                                fontSize: MediaQuery.of(context).size.width*0.05,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      child: Divider(),
+                    ),
+                    Text("Or connect with social"),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      child: Divider(),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding:
+                  EdgeInsets.only(left: MediaQuery.of(context).size.width*0.07, bottom: MediaQuery.of(context).size.width*0.05, right: MediaQuery.of(context).size.width*0.07,top: MediaQuery.of(context).size.width*0.03,),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(),
+                        child: SizedBox(
+                          width:  MediaQuery.of(context).size.width*0.07,
+                          height:  MediaQuery.of(context).size.width*0.07,
+                          child: Image.asset("images/facebook.png"),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left:MediaQuery.of(context).size.width*0.05),
+                        child: Text(
+                          "Facebook",
+                          style: TextStyle(
+                              fontSize:  MediaQuery.of(context).size.width*0.03,
+                              fontWeight: FontWeight.w400
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding:
+                  EdgeInsets.only(left: MediaQuery.of(context).size.width*0.07,right: MediaQuery.of(context).size.width*0.07),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(),
+                        child: SizedBox(
+                          width:  MediaQuery.of(context).size.width*0.07,
+                          height:  MediaQuery.of(context).size.width*0.07,
+                          child: Image.asset("images/google.png"),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left:MediaQuery.of(context).size.width*0.05),
+                        child: Text(
+                          "Google",
+                          style: TextStyle(
+                              fontSize:  MediaQuery.of(context).size.width*0.03,
+                              fontWeight: FontWeight.w400
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
-            ),
-            Expanded(
-              child: ListView(
-                children: <Widget>[
-                  Padding(
-                    padding:
-                    EdgeInsets.only(left: MediaQuery.of(context).size.width*0.07, bottom: MediaQuery.of(context).size.width*0.05, right: MediaQuery.of(context).size.width*0.07),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(),
-                          child: SizedBox(
-                            width:  MediaQuery.of(context).size.width*0.07,
-                            height:  MediaQuery.of(context).size.width*0.07,
-                            child: Image.asset("images/facebook.png"),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left:MediaQuery.of(context).size.width*0.05),
-                          child: Text(
-                            "Facebook",
-                            style: TextStyle(
-                                fontSize:  MediaQuery.of(context).size.width*0.03,
-                                fontWeight: FontWeight.w400
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                    EdgeInsets.only(left: MediaQuery.of(context).size.width*0.07,right: MediaQuery.of(context).size.width*0.07),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(),
-                          child: SizedBox(
-                            width:  MediaQuery.of(context).size.width*0.07,
-                            height:  MediaQuery.of(context).size.width*0.07,
-                            child: Image.asset("images/google.png"),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left:MediaQuery.of(context).size.width*0.05),
-                          child: Text(
-                            "Google",
-                            style: TextStyle(
-                                fontSize:  MediaQuery.of(context).size.width*0.03,
-                                fontWeight: FontWeight.w400
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
             )
           ],
         ),
