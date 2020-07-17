3eb103bb0808ce94362d7c45c531eb54df0541ff
Tuksa <emmydave414@gmail.com>
2020-07-17 16:44:09 +0100

    Minor adjustments

--- lib/home.dart
+++ lib/home.dart
@@ -284,20 +284,82 @@ class HomeBodyState extends State<HomeBody> {
         Consumer<LocationModel>(
           builder: (_, locationModel, __) {
             if (locationModel.mapMode == MapMode.DestinationNavigation) {
-              return Container(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        locationModel.dropOffLocationInfo.formattedAddress,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
+              return Material(
+                elevation: 12,
+                child: Container(
+                  color: Colors.white,
+                  height: 120,
+//                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      FlatButton(
+                        shape: CircleBorder(
+                            side: BorderSide(
+                                color: Colors.grey
+                            )
+                        ),
+                        child: Icon(
+                          Icons.clear,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          //TODO
+                        },
                       ),
-                    ),
-                    Text("ETA: $timeOfArrival")
-                  ],
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Text(
+                            '$timeOfArrival',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700
+                            ),
+                          ),
+                          Text(
+                            '8.4 km  3:39 pm'
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          FlatButton(
+                            shape: CircleBorder(
+                                side: BorderSide(
+                                    color: Colors.grey
+                                )
+                            ),
+                            child: Icon(
+                              Icons.call_split,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {
+                              //TODO
+                            },
+                          ),
+                          FlatButton(
+                            shape: CircleBorder(
+                                side: BorderSide(
+                                    color: Colors.grey
+                                )
+                            ),
+                            child: Icon(
+                              Icons.keyboard_arrow_up,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {
+                              //TODO
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               );
             }
